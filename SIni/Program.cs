3e0999908b2cbc37cead3af54af02eee1e7ba27d
Hell4Ge <sobiech12118@gmail.com>
2014-02-20 04:58:09 +0100

    Initial release

---
+++ SIni/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharpFile;
+
+namespace SIni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SIni/SharpIni/SharpIni.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharpFile;
+using System.IO;
+
+namespace SharpIni
+{
+    class SharpIni
+    {
+        private String fName = "";
+        SF sf = null;
+        public SharpIni(String fileName)
+        {
+            this.fName = fileName;
+            if (!File.Exists(fName))
+                File.Create(fName);
+                
+            sf = new SF(fName);
+        }
+
+        public void CreateSection(String section)
+        {
+            if(sf.OccurencesOf(section) == 0)
+                sf.Append(section);
+        }
+
+        public void CreateKey(String section, String key, String value)
+        {
+            
+        }
+
+        public void SetKey(String section, String key, String value)
+        {
+        }
+    }
+}
